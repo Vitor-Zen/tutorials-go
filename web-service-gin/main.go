473dc306927189216be43abd3551548b46fe69d3
Vitor-Zen <vitorzen19@gmail.com>
2024-09-06 09:30:14 -0300

    get element by id done

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -23,7 +23,10 @@ var alunos = []aluno{
 func main() {
 	// Criação do servidor
 	router := gin.Default()
+
+	// Criação das rotas
 	router.GET("/alunos", getAlunos)
+	router.GET("/alunos/:id", getAlunoByID)
 	router.POST("/alunos", postAlunos)
 
 	router.Run("localhost:8080")
@@ -31,19 +34,28 @@ func main() {
 
 // c*gin.Context nada mais é do que um ponteiro para Context(parte mais importante do GIN)
 func getAlunos(c *gin.Context) {
-	//C.IndentedJSON -> reponsavel por serializar nossa resposta em formato JSON
-	//http.StatusOK ->
-	c.IndentedJSON(http.StatusOK, alunos)
+	c.IndentedJSON(http.StatusOK, alunos) // reponsavel por serializar nossa resposta em formato JSON
 }
 
 func postAlunos(c *gin.Context) {
 	var newAluno aluno // Cria variavel newAluno que é do tipo aluno (struct criado no topo do código)
 
-	// Tenta pegar os dados da requisição e coloca esses dados em newAluno; Caso não de certo, da um return (não faz nada)
-	if err := c.BindJSON(&newAluno); err != nil {
+	if err := c.BindJSON(&newAluno); err != nil { // 	// Tenta pegar os dados da requisição e coloca esses dados em newAluno; Caso não de certo, da um return (não faz nada)
 		return
 	}
 
 	alunos = append(alunos, newAluno)            // Adiciona o newAluno no final da lista alunos
 	c.IndentedJSON(http.StatusCreated, newAluno) // Envia de volta uma resposta em formato JSON
+}
+
+func getAlunoByID(c *gin.Context) {
+	id := c.Param("id") // Pega o ID da requisição
+
+	for _, a := range alunos { // Ignora o indice e checa cada elemento do slice alunos
+		if a.ID == id { // Se o id passado for igual ao id do elemento,
+			c.IndentedJSON(http.StatusOK, a) // retorna o objeto a no formato JSON
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "alunos não encontrados"})
 }
